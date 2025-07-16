889167bff33eec5a436ba8f0a035508f48a0f431
Mark <suguitanmark123@gmail.com>
2025-07-16 17:02:38 +0800

    Implementing full  Animations and another sections card

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import Hero from "./sections/Hero"
 import NavBar from "./components/NavBar"
+import ShowcaseSection from "./sections/ShowcaseSection"
+import LogoSection from "./sections/LogoSection"
+import FeatureCards from "./sections/FeatureCards"
+import ExperienceSection from "./sections/ExperienceSection"
+import TechStack from "./sections/TechStack"
+import Testimonials from "./sections/Testimonials"
 
 const App = () => {
   return (
     <>
        <NavBar/>
        <Hero />
+       <ShowcaseSection/>
+       <LogoSection/>
+       <FeatureCards/>
+       <ExperienceSection/>
+       <TechStack/>
+       <Testimonials/>
     </>
   )
 }
